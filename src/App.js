85b8d362790fadd51a776a79ac82249d4945e0b9
softkle <kleiverbarros@gmail.com>
2022-05-19 08:43:14 -0500

    SECOND commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Testimonio from './componentes/Testimonio'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <div className="App">
+         <div className='contenedor-principal'>
+           <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+           <Testimonio
+           nombre='Emma Bostian'
+           pais='Suecia'
+           cargo='Ingeniero de software'
+           empresa='Spotify'
+           testimonio='Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify'
+           imagen='emma'
+           />
+        </div>
+      </div>
+   
   );
 }
 
